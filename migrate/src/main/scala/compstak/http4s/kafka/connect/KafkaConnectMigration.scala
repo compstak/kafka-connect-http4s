4a2f43c9aae46204ed9d4ef08880b739c60a1217
Cory <cory@goedelsoup.cs.int>
2019-12-26 13:34:03 -0500

    scalafmt

--- migrate/src/main/scala/compstak/http4s/kafka/connect/KafkaConnectMigration.scala
+++ migrate/src/main/scala/compstak/http4s/kafka/connect/KafkaConnectMigration.scala
@@ -17,49 +17,61 @@ import org.http4s.client.Client
 import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext
 
-final class KafkaConnectMigration[F[_]: ContextShift](client: KafkaConnectClient[F], config: KafkaConnectMigration.Configuration)(implicit val F: Sync[F]) {
+final class KafkaConnectMigration[F[_]: ContextShift](
+  client: KafkaConnectClient[F],
+  config: KafkaConnectMigration.Configuration
+)(implicit val F: Sync[F]) {
 
   def migrate: F[Unit] =
-    buildDeprecatedConfigs.map(_ ++ buildActiveConfigs)
+    buildDeprecatedConfigs
+      .map(_ ++ buildActiveConfigs)
       .flatMap { actions =>
-        actions.evalMap {
-          case Delete(name) => client.deleteConnector(name).void
-          case Upsert(name, config) => client.upsertConnector(name, config).void
-        }
-        .compile
-        .drain
-      }
-      
-  private[this] def listActiveConfigs = Stream
-    .fromIterator[F](Files.walk(config.path).iterator().asScala)
-    .filter(_.endsWith(".json"))
-
-  private[this] def buildActiveConfigs = listActiveConfigs
-    .evalMap(loadConfig)
-    .evalMap { case (name, conf) =>
-      conf.asObject match {
-        case Some(obj) =>
-          val isValidMap = obj.values.forall(_.isString)
-          if (!isValidMap) F.raiseError[MigrationAction](new Throwable("Configuration values must be strings"))
-          else {
-            val finalMap = obj.toMap.mapValues(_.asString).collect {
-              case (s, Some(c)) => s -> c
-            }
-              F.pure[MigrationAction](Upsert(name, finalMap))
+        actions
+          .evalMap {
+            case Delete(name)         => client.deleteConnector(name).void
+            case Upsert(name, config) => client.upsertConnector(name, config).void
           }
-        case None => F.raiseError[MigrationAction](new Throwable("Configuration must be a map"))
+          .compile
+          .drain
       }
-    }
+
+  private[this] def listActiveConfigs =
+    Stream
+      .fromIterator[F](Files.walk(config.path).iterator().asScala)
+      .filter(_.endsWith(".json"))
+
+  private[this] def buildActiveConfigs =
+    listActiveConfigs
+      .evalMap(loadConfig)
+      .evalMap {
+        case (name, conf) =>
+          conf.asObject match {
+            case Some(obj) =>
+              val isValidMap = obj.values.forall(_.isString)
+              if (!isValidMap) F.raiseError[MigrationAction](new Throwable("Configuration values must be strings"))
+              else {
+                val finalMap = obj.toMap.mapValues(_.asString).collect {
+                  case (s, Some(c)) => s -> c
+                }
+                F.pure[MigrationAction](Upsert(name, finalMap))
+              }
+            case None => F.raiseError[MigrationAction](new Throwable("Configuration must be a map"))
+          }
+      }
 
   private[this] def buildDeprecatedConfigs =
-    client.connectorNames.map { connectors =>
-      listActiveConfigs.map(_.getFileName.toString.replace(".json", "")).filter(!connectors.contains_(_))
-    }
-    .map(_.map(Delete(_)))
-    
+    client.connectorNames
+      .map { connectors =>
+        listActiveConfigs.map(_.getFileName.toString.replace(".json", "")).filter(!connectors.contains_(_))
+      }
+      .map(_.map(Delete(_)))
+
   private[this] def loadConfig(p: Path): F[(String, Json)] = {
-    val content = readInputStream(Sync[F].delay(this.getClass.getResourceAsStream(p.toAbsolutePath.toString)), 4096, Blocker.liftExecutionContext(ExecutionContext.global))
-      .through(utf8Decode)
+    val content = readInputStream(
+      Sync[F].delay(this.getClass.getResourceAsStream(p.toAbsolutePath.toString)),
+      4096,
+      Blocker.liftExecutionContext(ExecutionContext.global)
+    ).through(utf8Decode)
       .compile
       .lastOrError
 
@@ -71,7 +83,6 @@ final class KafkaConnectMigration[F[_]: ContextShift](client: KafkaConnectClient
 
 object KafkaConnectMigration {
 
-  
   def apply[F[_]: Sync: ContextShift](
     client: Client[F],
     uri: Uri,
@@ -83,7 +94,7 @@ object KafkaConnectMigration {
           new KafkaConnectMigration(c, _)
         )
       }
-      
+
   final case class Configuration(path: Path)
 
   object Configuration {
