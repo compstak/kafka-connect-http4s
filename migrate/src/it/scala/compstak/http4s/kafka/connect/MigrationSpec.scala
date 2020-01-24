e3e1d992de1d1bbdf5bca6e486c80e73590eacfa
Luka Jacobowitz <luka.jacobowitz@gmail.com>
2020-01-24 15:50:01 -0500

    Sleep between insert and get

--- migrate/src/it/scala/compstak/http4s/kafka/connect/MigrationSpec.scala
+++ migrate/src/it/scala/compstak/http4s/kafka/connect/MigrationSpec.scala
@@ -5,22 +5,23 @@ import org.scalatest.matchers.should.Matchers
 import org.http4s.client.asynchttpclient.AsyncHttpClient
 import org.http4s.Uri
 import cats.implicits._
-import cats.effect.IO
-import cats.effect.ContextShift
+import cats.effect.{ContextShift, IO, Resource, Timer}
+import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext
-import cats.effect.Resource
 
 class MigrationSpec extends AnyFunSuite with Matchers {
 
   implicit val ctx: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+  implicit val timer: Timer[IO] = IO.timer(ExecutionContext.global)
 
   test("After migration connector and config should exist") {
 
     AsyncHttpClient.resource[IO]()
       .flatMap(KafkaConnectMigration[IO](_, Uri.uri("http://localhost:18083")))
       .flatMap(migration => Resource.liftF(
-          migration.migrate 
-            *> migration.client.connectorNames.map(_ shouldBe List("test"))
+          migration.migrate *> 
+            IO.sleep(10.seconds) *> 
+            migration.client.connectorNames.map(_ shouldBe List("test"))
       ))
       .use(IO.pure)
       .unsafeRunSync()
