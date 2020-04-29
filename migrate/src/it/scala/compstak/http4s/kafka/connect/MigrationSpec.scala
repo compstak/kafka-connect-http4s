51257f8c532a17e4f893a3ba829e6f8a60a23e7b
Cory <cory@goedelsoup.cs.int>
2020-04-29 13:38:24 -0400

    fix test

--- migrate/src/it/scala/compstak/http4s/kafka/connect/MigrationSpec.scala
+++ migrate/src/it/scala/compstak/http4s/kafka/connect/MigrationSpec.scala
@@ -1,11 +1,13 @@
 package compstak.http4s.kafka.connect
 
+import cats.implicits._
+import cats.effect.{ContextShift, IO, Resource, Timer}
+import io.circe.literal._
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import org.http4s.client.asynchttpclient.AsyncHttpClient
 import org.http4s.Uri
-import cats.implicits._
-import cats.effect.{ContextShift, IO, Resource, Timer}
+
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext
 
@@ -17,7 +19,19 @@ class MigrationSpec extends AnyFunSuite with Matchers {
   test("After migration connector and config should exist") {
 
     AsyncHttpClient.resource[IO]()
-      .flatMap(KafkaConnectMigration[IO](_, Uri.uri("http://localhost:18083"), "test/kafka/connect"))
+      .flatMap(KafkaConnectMigration[IO](_, Uri.uri("http://localhost:18083"), Map(
+        "test" -> json"""
+        {
+          "connector.class": "io.debezium.connector.postgresql.PostgresConnector", 
+          "database.hostname": "database", 
+          "database.port": "5432", 
+          "database.user": "postgres", 
+          "database.password": "", 
+          "database.dbname" : "postgres", 
+          "database.server.name": "test"
+        }
+        """
+      )))
       .flatMap(migration => Resource.liftF(
           migration.migrate *> 
             IO.sleep(10.seconds) *> 
